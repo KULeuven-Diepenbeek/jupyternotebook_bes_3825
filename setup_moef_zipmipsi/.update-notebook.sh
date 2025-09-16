57951a360ca60d28b98acb9b14ed84dfa3392d59
Kris Aerts <Kris.Aerts@kuleuven.be>
2025-09-16 08:25:53 +0200

    Delete setup_moef_zipmipsi directory

--- setup_moef_zipmipsi/.update-notebook.sh
+++
@@ -1,9 +0,0 @@
-echo "Updating...";
-
-GITREPO="https://github.com/KULeuven-Diepenbeek/jupyter_BES_3825_exercises.git";
-GITHUB_DIR=../originele_oefeningen;
-UPDATEFILE=update_workspace.sh;
-
-mkdir $GITHUB_DIR > /dev/null 2>&1 && git clone $GITREPO $GITHUB_DIR/. > /dev/null 2>&1 && chmod +x $GITHUB_DIR/$UPDATEFILE > /dev/null 2>&1;
-chmod +w -R $GITHUB_DIR > /dev/null && cd $GITHUB_DIR && git reset --hard > /dev/null && git clean -fxd > /dev/null && git pull > /dev/null 2>&1 && chmod +x $GITHUB_DIR/$UPDATEFILE > /dev/null  && chmod -w -R $GITHUB_DIR > /dev/null;
-sh $GITHUB_DIR/$UPDATEFILE;
--- setup_moef_zipmipsi/README.md
+++
@@ -1,3 +0,0 @@
-# De setup van academiejaar 2022-'23 
-
-Niet te gebruiken dus, maar dient als reservekopie.
